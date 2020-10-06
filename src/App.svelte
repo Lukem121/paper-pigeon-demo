5f7a7ab52768ac4b332befdfe1cd325738132681
luke askew <lukeask@hotmail.co.uk>
2020-10-06 15:52:55 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,58 @@
+<script>
+	import { Discord } from 'paper-pigeon';
+	const send = new Discord("https://discordapp.com/api/webhooks/763031675361034260/Al45h0W9IbGDowO9kNzs0LtUGJWb-PQYzv_MbpqSSx_4YEERFoYhHzBIFAgsfKLd1gMD");
+
+	let text = "";
+
+	const handleNormal = () => {
+		if(text != ""){
+			send.message(text);
+		}
+	}
+	const handleSuccess = () => {
+		if(text != ""){
+			send.success(text);
+		}
+	}
+	const handleError = () => {
+		if(text != ""){
+			send.error(text);
+		}
+	}
+</script>
+
+<main>
+	<span>1. </span><a href="https://discord.gg/rP6RmNK">Join the discord</a>
+	<br>
+	<br>
+	<span>2. </span>
+	<input bind:value={text} type="text">
+	<br>
+	<br>
+	<span>3. </span>
+	<button on:click={handleNormal} style="background: #007bff;" >Normal</button>
+	<button on:click={handleSuccess} style="background: #28a745;" >Success</button>
+	<button on:click={handleError} style="background: #dc3545;">Error</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	button{
+		cursor: pointer;
+		color: white;
+		border-radius: .25rem;
+		border: 1px solid transparent;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
