264a35e878acf4f07d85fdcc16a02db974424898
luke askew <lukeask@hotmail.co.uk>
2020-10-06 21:49:18 +0100

    Added Slack to demo

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script>
-	import { Discord } from 'paper-pigeon';
+	import { Discord, Slack } from 'paper-pigeon';
 	const send = new Discord("https://discordapp.com/api/webhooks/763031675361034260/Al45h0W9IbGDowO9kNzs0LtUGJWb-PQYzv_MbpqSSx_4YEERFoYhHzBIFAgsfKLd1gMD");
+	const sendSlack = new Slack("https://hooks.slack.com/services/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");
 
+	//Discord
 	let text = "";
 
 	const handleNormal = () => {
@@ -19,10 +21,29 @@
 			send.error(text);
 		}
 	}
+	//Slack
+	let textSlack = "";
+
+	const handleNormalSlack = () => {
+		if(textSlack != ""){
+			sendSlack.message(textSlack);
+		}
+	}
+	const handleSuccessSlack = () => {
+		if(textSlack != ""){
+			sendSlack.success(textSlack);
+		}
+	}
+	const handleErrorSlack = () => {
+		if(textSlack != ""){
+			sendSlack.error(textSlack);
+		}
+	}
 </script>
 
 <main>
-	<span>1. </span><a href="https://discord.gg/rP6RmNK">Join the discord</a>
+	<h2>Discord</h2>
+	<span>1. </span><a target="_blank" href="https://discord.gg/rP6RmNK">Join the Discord</a>
 	<br>
 	<br>
 	<span>2. </span>
@@ -33,6 +54,23 @@
 	<button on:click={handleNormal} style="background: #007bff;" >Normal</button>
 	<button on:click={handleSuccess} style="background: #28a745;" >Success</button>
 	<button on:click={handleError} style="background: #dc3545;">Error</button>
+
+	<hr>
+
+	<br>
+	<h2>Slack</h2>
+	<span>1. </span><a target="_blank" href="https://rashek.slack.com/archives/C01BW8TMXE2">Join the Slack channels</a>
+	<br>
+	<br>
+	<span>2. </span>
+	<input bind:value={textSlack} type="text">
+	<br>
+	<br>
+	<span>3. </span>
+	<button on:click={handleNormalSlack} style="background: #007bff;" >Normal</button>
+	<button on:click={handleSuccessSlack} style="background: #28a745;" >Success</button>
+	<button on:click={handleErrorSlack} style="background: #dc3545;">Error</button>
+
 </main>
 
 <style>
